8205368fd5652ee9998ccbef14b8d19fec465cef
ChangMinPark2 <98qkrckdals@naver.com>
2024-01-22 15:16:46 +0900

    feat(dom): DomInterface 실습

---
+++ dom-interface/script.js
@@ -0,0 +1,3 @@
+document.getElementById("dummy");
+console.log(typeof document); //다큐먼트 형태로 출력 (document 의 타입은 Object이다.)
+console.log(document.URL);// 다큐먼트가 자바스크립트의 오브젝트 형태이므로, 키(url)로 프로퍼티 값을 구할 수 있다.
